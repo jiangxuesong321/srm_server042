ef55350ff2a2395f9635c7d61cb67b4aac35c49f
jiangxuesong321 <1393143765@qq.com>
2023-05-29 22:07:01 +0800

    代码整合

--- jeecg-boot-module-system/src/main/java/com/cmoc/modules/srm/mapper/PurchaseOrderAndSapDocumentMapper.java
+++
@@ -1,7 +0,0 @@
-package com.cmoc.modules.srm.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cmoc.modules.srm.entity.PurchaseOrderAndSapDocument;
-
-public interface PurchaseOrderAndSapDocumentMapper extends BaseMapper<PurchaseOrderAndSapDocument> {
-}
--- jeecg-boot-module-system/src/main/java/com/cmoc/modules/system/mapper/PurchaseOrderMainMapper.java
+++
@@ -1,7 +0,0 @@
-package com.cmoc.modules.system.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cmoc.modules.system.entity.PurchaseOrderMain;
-
-public interface PurchaseOrderMainMapper extends BaseMapper<PurchaseOrderMain> {
-}
